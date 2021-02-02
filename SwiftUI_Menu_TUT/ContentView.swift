2af3226f3962d0a161ae601a78a00114578d79cd
SeonJungKim <ksj083895@gmail.com>
2021-02-02 12:09:00 +0500

    Menu

--- SwiftUI_Menu_TUT/ContentView.swift
+++ SwiftUI_Menu_TUT/ContentView.swift
@@ -9,8 +9,48 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        Menu {
+            Button("Cancel", action: {})
+            //버튼 아래에 메뉴를 한번 더 추가해준다.
+            Menu("More") {
+                Button(action: {
+                }) {
+                    HStack {
+                        Text("Rename")
+                        Image(systemName: "plus.circle")
+                    }
+                }
+                
+                //메뉴안에 아이콘을 추가해준다.
+                Button(action: {
+                }) {
+                    HStack {
+                        Text("Developer mode")
+                        Image(systemName: "person.crop.circle.fill")
+                    }
+                }
+            }
+            Section {
+                Button(action: {
+                }) {
+                    HStack {
+                        Text("Serch")
+                        Image(systemName: "magnifyingglass")
+                    }
+                }
+                Button(action: {
+                }) {
+                    HStack {
+                        Text("Add")
+                        Image(systemName: "plus")
+                    }
+                }
+            }
+            //최상단 메뉴 버튼
+        } label: {
+            Label("Create", systemImage: "plus")
+        }
     }
 }
 
